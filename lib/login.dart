c29ac55ffa432b6ac05bc06e2bf4412afc86781c
Your Name <viccodwipramadian@gmail.com>
2020-08-03 18:11:38 +0800

    First2

--- lib/login.dart
+++ lib/login.dart
@@ -5,18 +5,21 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
 
   final _formKey = GlobalKey<FormState>();
-  String _Email,_Password;
+  String _Email, _Password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        height: MediaQuery.of(context).size.height,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image(image: AssetImage("images/LOGO_KABUPATEN_TANAH_BUMBU.png"),
+            Image(image: AssetImage("images/Intro_1.png"),
               height: 150,
             ),
             SizedBox(height: 50.0,),
@@ -46,8 +49,9 @@ class LoginPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(40.0),
                             )
                         ),
-                        validator: (input) => !input.contains('@')
-                            ?'silahkan masukan Email anda'
+                        validator: (input) =>
+                        !input.contains('@')
+                            ? 'silahkan masukan Email anda'
                             : null,
                         onSaved: (input) => _Email = input,
                       ),
@@ -73,8 +77,9 @@ class LoginPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(40.0),
                             )
                         ),
-                        validator: (input) => input.length < 8
-                            ?'Kata Sandi yang harus di masukan minimal 8 angka'
+                        validator: (input) =>
+                        input.length < 8
+                            ? 'Kata Sandi yang harus di masukan minimal 8 angka'
                             : null,
                         onSaved: (input) => _Password = input,
                         obscureText: true,
@@ -86,8 +91,7 @@ class LoginPage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         GestureDetector(
-                          onTap: () {
-//                            },
+                          onTap: () {},
                           child: Container(
                             padding: EdgeInsets.all(10.0),
                             decoration: BoxDecoration(
@@ -105,44 +109,13 @@ class LoginPage extends StatelessWidget {
                           ),
                         ),
                         SizedBox(width: 20,),
-                        _signInButton(),
+
                       ],
                     )
 
                   ],
                 )
             ),
-          ],
-        ),
-      ),
-    );
-  }
-  Widget _signInButton() {
-    return OutlineButton(
-      onPressed: () {
-
-      },
-      splashColor: Colors.grey,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-      highlightElevation: 0,
-      borderSide: BorderSide(color: Colors.black),
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(image: AssetImage("images/google_logo.png"),height: 25.0,),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                'Masuk Dengan Google',
-                style: TextStyle(
-                  fontSize: 10.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
           ],
         ),
       ),
